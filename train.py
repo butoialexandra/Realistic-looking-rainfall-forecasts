8b77f40522567261c1234ffc377a0e02c820a021
Alexandra Butoi <acbutoi1@sheffield.ac.uk>
2020-11-10 02:47:01 +0100

    added tensorboard visualisation

--- train.py
+++ train.py
@@ -1,19 +1,39 @@
 import argparse
 import numpy as np
+import datetime
+import matplotlib.pyplot as plt
 
 import torch
+import torchvision
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
 
 from discriminator import Discriminator
 from generator import Generator
 from dataset import WGANDataset
 
+def sample_image(generator, cuda, n_row, latent_dim, img_shape):
+    FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row, latent_dim))))
+
+    gen_imgs = generator(z, img_shape)
+    # gen_imgs = gen_imgs.unsqueeze(1)
+
+    # fig = plt.figure(figsize=(30, 3))
+    # grid = torchvision.utils.make_grid(gen_imgs, nrow=n_row)
+    # plt.imshow(grid.permute(1, 2, 0), cmap='viridis')
+    # plt.colorbar(orientation='horizontal')
+    fig, axs = plt.subplots(ncols=n_row, figsize=(5*n_row, 5))
+    for i in range(gen_imgs.shape[0]):
+        axs[i].pcolormesh(gen_imgs[i,:,:].data, cmap='viridis')
+    return fig
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_epochs", type=int, default=50, help="number of epochs of training")
-    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--batch_size", type=int, default=16, help="size of the batches")
     parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
@@ -48,11 +68,17 @@ if __name__ == "__main__":
 
     Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
+    writer = SummaryWriter('runs/{}'.format(datetime.datetime.now()))
+
     # ----------
     #  Training
     # ----------
     batches_done = 0
+
+    print('Started Training')
     for epoch in range(opt.n_epochs):
+        running_loss_G = 0.0
+        running_loss_D = 0.0
 
         for i, imgs in enumerate(dataloader):
 
@@ -75,6 +101,7 @@ if __name__ == "__main__":
 
             loss_D.backward()
             optimizer_D.step()
+            running_loss_D += loss_D.item()
 
             # Clip weights of discriminator
             for p in discriminator.parameters():
@@ -95,6 +122,7 @@ if __name__ == "__main__":
 
                 loss_G.backward()
                 optimizer_G.step()
+                running_loss_G += loss_D.item()
 
                 print(
                     "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
@@ -102,6 +130,11 @@ if __name__ == "__main__":
                     epoch, opt.n_epochs, batches_done % len(dataloader), len(dataloader), loss_D.item(), loss_G.item())
                 )
 
-            # if batches_done % opt.sample_interval == 0:
-            #     save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
             batches_done += 1
+
+        writer.add_scalar('Discriminator loss', running_loss_D / len(dataloader), epoch)
+        writer.add_scalar('Generator loss', running_loss_G / (len(dataloader) // opt.n_critic), epoch)
+        writer.add_figure('Generated images', sample_image(generator, cuda, 10, opt.latent_dim, opt.img_shape),
+                          global_step=epoch)
+
+    print('Finished Training')
