181b17a3f7ff5127606e359bb6a1b49b06783ddc
Matus Zilinec <zilinec.m@gmail.com>
2020-11-13 13:36:45 +0100

    Add new generator code

--- cgan.py
+++ cgan.py
@@ -11,27 +11,30 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torch.autograd import Variable
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
+
+import matplotlib.pyplot as plt
 
 from dataset import Dataset
+from generator import Generator
 
 
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--latent_dim", type=int, default=1000, help="dimensionality of the latent space")
 parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=500, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=10, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
 
@@ -52,43 +55,6 @@ else:
 device = torch.device("cuda:0" if cuda else "cpu")
 
 
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            # if normalize:
-                # layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
-
-        in_pixels = int(np.prod(input_shape))
-        out_pixels = int(np.prod(output_shape))
-
-        self.model = nn.Sequential(
-            *block(opt.latent_dim + in_pixels, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, out_pixels),
-            nn.Tanh()
-        )
-
-    def forward(self, noise, observation):
-        # Concatenate label embedding and image to produce input
-        # Keep only batch size and flatten everything else
-        observation = observation.view(observation.size(0), in_pixels)#-1)
-        # print("A",observation.size())
-        gen_input = torch.cat((observation, noise), -1)
-        # print("B",gen_input.size())
-        img = self.model(gen_input)
-        # print("C",img.size())
-        img = img.view(img.size(0), *output_shape)
-        # print("D",img.size())
-        return img
-
-
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
@@ -97,7 +63,9 @@ class Discriminator(nn.Module):
         out_pixels = int(np.prod(output_shape))
 
         self.model = nn.Sequential(
-            nn.Linear(out_pixels + in_pixels, 512),
+            # nn.Conv2d(in_channels=2, out_channels=1, kernel_size=5, stride=1),
+            # nn.Linear(out_pixels + in_pixels, 512),
+            nn.Linear(out_pixels, 512),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 512),
             nn.Dropout(0.4),
@@ -110,9 +78,13 @@ class Discriminator(nn.Module):
 
     def forward(self, img, observation):
         # Concatenate label embedding and image to produce input
-        observation = observation.view(observation.size(0), -1)
-        d_in = torch.cat((img.view(img.size(0), -1), observation), -1)
+        # observation = observation.view(observation.size(0), -1)
+        # print(observation.shape, img.shape)
+        # d_in = torch.cat((img.view(img.size(0), -1), observation), -1)
+        # d_in = torch.stack((observation, img), dim=1)
+        d_in = img.view(img.size(0), -1)
         validity = self.model(d_in)
+        validity = torch.sigmoid(validity)
         return validity
 
 
@@ -151,7 +123,7 @@ FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
-def sample_image(training_data, n_row, batches_done):
+def sample_image_(training_data, n_row, batches_done):
     """Saves a grid of generated images"""
     n_row = 1 # FIXME
     # Sample noise
@@ -166,7 +138,30 @@ def sample_image(training_data, n_row, batches_done):
     gen_imgs = generator(z, y_pred)
     gen_imgs = gen_imgs.unsqueeze(1)
     # print(gen_imgs.data.size())
-    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+    # data = torch.cat((gen_imgs.data.squeeze(0), y_real), dim=0)
+    save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=2, normalize=True)
+
+
+def sample_image(training_data, n_row, batches_done):
+    n_row = 1 # FIXME
+    # Sample noise
+    z = Variable(FloatTensor(np.random.normal(0, 1, (n_row, opt.latent_dim))))
+    # Get labels ranging from 0 to n_classes for n rows
+    y_pred, y_real = training_data.get_x_y_by_id(training_data.selected_indices[0])  # TODO: fix first date from 201805
+    y_pred = torch.tensor(y_pred, device=device).repeat(n_row, 1, 1) / 10
+    y_real = torch.tensor(y_real, device=device).repeat(n_row, 1, 1) / 10
+    gen_imgs = generator(z, y_pred)
+
+    fig = plt.figure(figsize=(6, 3.2))
+    ax = fig.add_subplot(121)
+    ax.set_title('Output')
+    plt.imshow(gen_imgs.squeeze().detach().cpu())
+
+    ax = fig.add_subplot(122)
+    ax.set_title('Observation')
+    plt.imshow(y_real.squeeze().detach().cpu())
+    plt.colorbar(orientation='horizontal')
+    plt.savefig("images/%d.png" % batches_done)
 
 
 # ----------
@@ -188,8 +183,8 @@ for epoch in range(opt.n_epochs):
         fake = Variable(FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
 
         # Configure input
-        real_imgs = Variable(real_imgs.type(FloatTensor))
-        pred_imgs = Variable(pred_imgs.type(FloatTensor))
+        real_imgs = Variable(real_imgs.type(FloatTensor)) / 10
+        pred_imgs = Variable(pred_imgs.type(FloatTensor)) / 10
 
         # -----------------
         #  Train Generator
@@ -219,10 +214,14 @@ for epoch in range(opt.n_epochs):
 
         # Loss for real images
         validity_real = discriminator(real_imgs, pred_imgs)
+        print("Validity real:", validity_real)
         d_real_loss = adversarial_loss(validity_real, valid)
 
         # Loss for fake images
-        validity_fake = discriminator(gen_imgs.detach(), pred_imgs)
+        fake_imgs = gen_imgs.detach()
+        # fake_imgs = torch.zeros_like(fake_imgs) # TODO: remove me!!!!!
+        validity_fake = discriminator(fake_imgs, pred_imgs)
+        print("Validity fake:", validity_fake)
         d_fake_loss = adversarial_loss(validity_fake, fake)
 
         # Total discriminator loss
---
+++ generator.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+
+import util
+from config import input_shape, output_shape
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            # if normalize:
+                # layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.in_pixels = int(np.prod(input_shape))
+        self.out_pixels = int(np.prod(output_shape))
+
+        self.model = nn.Sequential(
+            *block(1000 + 0
+            # self.in_pixels
+            , 1024, normalize=False),
+            *block(1024, 1024),
+            # *block(2560, 512),
+            # *block(512, 1024),
+            nn.Linear(1024, self.out_pixels),
+            # nn.Tanh()
+            nn.Sigmoid(),
+        )
+
+    def forward(self, noise, observation):
+        # Concatenate label embedding and image to produce input
+        # Keep only batch size and flatten everything else
+        observation = observation.view(observation.size(0), self.in_pixels)#-1)
+        # print("A",observation.size())
+        # gen_input = torch.cat((observation, noise), -1)
+        gen_input = noise
+        # print("B",gen_input.size())
+        img = self.model(gen_input)
+        # print("C",img.size())
+        img = img.view(img.size(0), *output_shape)
+        # print("D",img.size())
+        return img
+
+
+class GeneratorA(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = nn.Sequential(*self._build_encoder())
+        self.decoder = nn.Sequential(*self._build_decoder())
+        self.alt_decoder = nn.Sequential(*self._build_alt_decoder())
+
+    def _build_encoder(self):
+        layers = [
+            # (batch_size, 1, 127, 188) -> 127x188
+            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1),
+            # (batch_size, 1, h-2, w-2) -> 125x186
+            nn.MaxPool2d(kernel_size=3, stride=3),
+            # (batch_size, 1, floor((h-2)/3), floor((w-2)/3)) -> 41x62
+            util.Flatten(),
+            # (batch_size, 41*62) -> 2542
+            nn.Linear(in_features=41*62, out_features=1024),
+            nn.Sigmoid(),
+        ]
+        return layers
+
+    def _build_alt_decoder(self):
+        layers = [
+            nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=3, stride=3),
+            # nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=4, stride=1),
+        ]
+        return layers
+    
+    def _build_decoder(self):
+        # Deconvolution: 
+        # output_size = stride * (input_size -1) + kernel_size - 2*pad
+        layers = [
+            # (batch_size, 1024)
+            nn.Linear(in_features=1024, out_features=2623),
+            nn.Sigmoid(),
+            # util.View((1, 59, 85)), # and 5015?
+            util.View((1, 43, 61)),  # for 2623
+            nn.ConvTranspose2d(in_channels=1, out_channels=1, kernel_size=7, stride=7, padding=0)
+            # 295, 427
+        ]
+        return layers
+
+    def forward(self, noise, input):
+        assert input.shape[1:] == input_shape, (input.shape, input_shape)
+        # embedding = self.encoder(input.unsqueeze(1))
+        # assert embedding.shape[1] == 1024, embedding.shape
+        # output = self.decoder(embedding).squeeze(1)
+        # output = output[:,:295,:]  # we just have to get rid of 6 extra elements here
+
+        output = self.alt_decoder(input.unsqueeze(1)).squeeze(1)
+        output = output[:,:295, :427]
+        assert output.shape[1:] == output_shape, (output.shape, output_shape)
+        print(output.shape)
+        return output.contiguous()
